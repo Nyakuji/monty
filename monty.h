c0b96c08f22974ec9055bf067b1221e1c64a5d65
Philip Opiyo <opiyophilipo@gmail.com>
2023-10-19 08:14:35 -0700

    monty

---
+++ monty.h
@@ -0,0 +1,105 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+/**
+ * struct globals - global structure to use in the functions
+ * @lifo: is stack or queue
+ * @cont: current line
+ * @arg: second parameter inside the current line
+ * @head: doubly linked list
+ * @fd: file descriptor
+ * @buffer: input text
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct globals
+{
+	int lifo;
+	unsigned int cont;
+	char  *arg;
+	stack_t *head;
+	FILE *fd;
+	char *buffer;
+} global_t;
+
+extern global_t programContext;
+
+/* mystr_fun.c */
+int _strcmp(char *s1, char *s2);
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+
+/* doubly_fun.c */
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+stack_t *add_dnodeint(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
+
+/* malloc_fun.c */
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* get_function */
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+
+/* main.c */
+void free_programContext(void);
+void start_programContext(FILE *fd);
+FILE *check_input(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+/* opcode_instructions */
+void _push(stack_t **doubly, unsigned int line_number);
+void _pall(stack_t **doubly, unsigned int line_number);
+void _pint(stack_t **doubly, unsigned int line_number);
+void _pop(stack_t **doubly, unsigned int line_number);
+void _swap(stack_t **doubly, unsigned int line_number);
+void _queue(stack_t **doubly, unsigned int line_number);
+void _stack(stack_t **doubly, unsigned int line_number);
+void _add(stack_t **doubly, unsigned int line_number);
+void _nop(stack_t **doubly, unsigned int line_number);
+void _sub(stack_t **doubly, unsigned int line_number);
+#endif
